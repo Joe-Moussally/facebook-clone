7be262a2f3a80581278ea8dee3badcf1efbc1a57
MJoe111 <joemoussally111@gmail.com>
2022-06-03 12:59:00 +0300

    Adding to profile click

--- js/home.js
+++ js/home.js
@@ -8,6 +8,19 @@ let profiles = document.getElementsByClassName('profile');
 
 for (let i=0; i<profiles.length; i++) {
     console.log(profiles[i].id)
+    profiles[i].addEventListener('click', () => {
+
+        let data = new FormData()
+        data.append('id',profiles[i].id)
+
+        axios({
+            method: 'POST',
+            url: 'http://facebook/profile.php',
+            data: data
+        }).then((Response) => {
+            console.log(Response)
+        })
+    })
 }
 
 console.log(profiles)
