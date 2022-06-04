19592c69166d7b1a0fb02672885d83c63e18d8b6
MJoe111 <joemoussally111@gmail.com>
2022-06-04 18:04:20 +0300

    Fixing nav

---
+++ js/nav.js
@@ -0,0 +1,15 @@
+let profile_nav = document.getElementById('profile-nav');
+let logged_in_id = localStorage.getItem('user_id');
+let datanav = new FormData();
+
+datanav.append('id',logged_in_id);
+
+axios({
+    method: 'POST',
+    url: 'http://facebook/profile.php',
+    data:datanav
+}).then((Response) => {
+    profile_nav.src = Response.data[0].profile_picture;
+    console.log(Response.data[0].id)
+    profile_nav.parentElement.id = Response.data[0].id
+})
