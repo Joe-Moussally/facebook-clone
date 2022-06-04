827ac6ba304a47ed75371aca6a5f5926393e61e7
MJoe111 <joemoussally111@gmail.com>
2022-06-04 18:51:34 +0300

    Fixing profile.js

--- js/nav.js
+++ js/nav.js
@@ -11,7 +11,12 @@ axios({
 }).then((Response) => {
     profile_nav.src = Response.data[0].profile_picture;
     console.log(Response.data[0].id)
-    profile_nav.parentElement.id = Response.data[0].id
+})
+
+//adding redirect to profile
+profile_nav.addEventListener('click', () => {
+    localStorage.setItem('profile_id', logged_in_id);
+    window.location.href = "http://facebook/html/profile.html"
 })
 
 //adding logout
